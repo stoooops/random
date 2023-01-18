cb15dc4980a9f25e30d104bd14d3129792701f21
Cory Gabrielsen <corygabrielsen@gmail.com>
2023-01-17 22:40:11 -0800

    Convert to gifv at end

--- youtube-gif.sh
+++ youtube-gif.sh
@@ -9,6 +9,7 @@ python3 -m venv "$VENV_NAME"
 source "$VENV_NAME/bin/activate"
 
 # install packages
+type gifsicle || sudo apt-get install -y gifsicle
 pip install youtube_dl
 pip install imageio
 pip install imageio_ffmpeg
--- youtube-gif/main.py
+++ youtube-gif/main.py
@@ -9,30 +9,48 @@ def convert_to_gif(
     video_id: str, start_time: float, end_time: float, output_file: str
 ) -> None:
     # Download the video using youtube_dl
-    ydl_opts: Dict[str, str] = {"outtmpl": "temp.%(ext)s"}
+    ydl_opts: Dict[str, str] = {"outtmpl": f"temp.{video_id}.%(ext)s"}
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([f"https://www.youtube.com/watch?v={video_id}"])
 
-    reader: Any = imageio.get_reader("temp.mp4")
+    # files
+    mp4: str = f"temp.{video_id}.mp4"
+    gif: str = output_file.split(".")[0] + ".gif"
+    gifv = output_file.split(".")[0] + ".gifv"
+
+    print("Converting video to GIF...")
+    reader: Any = imageio.get_reader(mp4)
     fps: Any = reader.get_meta_data()["fps"]
-    writer: Any = imageio.get_writer(output_file, fps=fps)
+    writer: Any = imageio.get_writer(gif, fps=fps)
     for i, frame in enumerate(reader):
+        if i % fps == 0:
+            print(f"Processed {i} frames ({i/fps}s)")
         if i / fps < start_time:
             continue
         if i / fps > end_time:
             break
         writer.append_data(frame)
     writer.close()
-    # Delete the temporary video file
-    # os.remove("temp.mp4")
+
+    # convert gif to gifv
+    print("Converting GIF to GIFV...")
+    os.system(f"gifsicle --colors 256 --optimize=3 {gif} -o {gifv}")
 
 
-if __name__ == "__main__":
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("video_id", help="YouTube video ID")
     parser.add_argument("start_time", type=float, help="Start timestamp in seconds")
     parser.add_argument("end_time", type=float, help="End timestamp in seconds")
     parser.add_argument("output_file", help="Output file name (e.g. output.gif)")
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_args()
 
     convert_to_gif(args.video_id, args.start_time, args.end_time, args.output_file)
+
+
+if __name__ == "__main__":
+    main()
